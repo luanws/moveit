e44c30cc7e60af00db26ad6dbe9fe03730ea058f
luanws <luan.w.silveira@gmail.com>
2021-02-23 12:19:34 -0300

    document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,6 @@
-import Head from 'next/head'
 import '../styles/global.css'
 import '../components/ExperienceBar/styles.css'
 
 export default function App({ Component, pageProps }: any) {
-  return (
-    <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
