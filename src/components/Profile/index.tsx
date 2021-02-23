58fa2173e973c3f0dc19e72ddae83d687a4aa8de
luanws <luan.w.silveira@gmail.com>
2021-02-23 14:00:40 -0300

    Update index.tsx

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -3,11 +3,14 @@ const styles = require('./styles.module.css')
 export default function Profile() {
   return (
     <div className={styles.container}>
-      <img src="https://github.com/luanws.png" alt="Usuário"/>
+      <img src="https://github.com/luanws.png" alt="Usuário" />
       <div>
-        <strong>Luan Silveira</strong>
-        <p>Level 1</p>
+        <strong>Nome</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level 1
+        </p>
       </div>
     </div>
-  ) 
+  )
 }
